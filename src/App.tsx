bbff8dcf0da68dde65c8b3cccfef6d42d2e01ed5
yogita <sabaley16@gmail.com>
2024-10-24 22:02:40 +0530

    converted in function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,42 @@
-import { Component, useEffect, useState } from "react";
+import { Component, useEffect, useState,ChangeEvent } from "react";
 import CardList from "./components/card-list/cardList.component";
 import SearchBox from "./components/search-box/searchBox.component";
 import './App.css'
 import { getData } from "./utils/data.utils";
 
 const App = () => {
-  const [monsters, setMonsters] = useState([])
+  const [monsters, setMonsters] = useState<Monster[]>([])
   const [searchString, setSearchString] = useState('')
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
   type Monster = {
     id: string;
     name: string;
     email: string;
   }
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then((response) => response.json())
-  //     .then((result) => this.setState(
-  //       () => {
-  //         this.setState({ monsters: result })
-  //       }
-  //     ))
-  //   // .then((res)=>this.setState({users:res}))
-  // }
+
   useEffect(() => {
     const fetchUsers = async () => {
       const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users')
+      setMonsters(users)
     }
+    fetchUsers()
   }, [])
 
-  const onSearchChange = (event) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchString);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchString]);
+
+  const onSearchChange = (event:ChangeEvent<HTMLInputElement>):void => {
     const searchString = event.target.value.toLowerCase()
-    // this.setState(
-    //   () => {
-    //     return { searchString: searchString }
-    //   }
-    // )
     setSearchString(searchString)
   }
 
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLowerCase().includes(searchString)
-  })
+  
   return (
     <>
       <h1 className="app-title">Monsters Rolodex</h1>
